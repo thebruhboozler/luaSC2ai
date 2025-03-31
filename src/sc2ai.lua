ca0d15a5993e4e79b67b27b016d433ffdec8ea5b
thebruhboozler <sababugs112@gmail.com>
2025-03-31 16:59:52 +0400

    added documentation

--- src/sc2ai.lua
+++ src/sc2ai.lua
@@ -1,3 +1,5 @@
+---SC2AI MODULE
+-- @module sc2ai 
 local pb = require("pb")
 local protoc = require("protoc")
 local socket = require("socket")
@@ -9,6 +11,15 @@ local actionHelper = require("src.actionHelper")
 local sc2ai = {}
 sc2ai.__index = sc2ai
 
+
+
+
+--- creates sc2ai agent object 
+--- @param name string name of the bot
+--- @param race nmber race of the bot (1 for terran , 2 for zeg , 3 for protos and 4 for random)
+--- @param ip ?string|nil ip where starcraft 2 expects the connection , if nil then it will use the environment varriable SC2IP
+--- @param port ?string|nil port where starcraft 2 expects the connection  , if nil then it will use the environment varriable SC2PORT
+--- @return table agent 
 function sc2ai.new(name, race , ip, port)
 
 	if ip == nil then 
@@ -58,6 +69,14 @@ function sc2ai.new(name, race , ip, port)
 	return self
 end
 
+
+---tells starcraft 2 to create the match 
+---@param mapPath string path to the .SC2MAP file 
+---@param enemyRace number race of the opponent ( 1 Terran , 2 Zerg  , 3 Protos , 4 random)
+---@param autoJoin ?boolean|nil  will automatically join the game upon the match being created , by default is equal to true
+---@param disableFog ?boolean|nil will disable fog , by default is equal to true
+---@param randomSeed ?number|nil the seed which starcraft 2 will use , if not passed starcraft 2 will use its own seed
+---@param realtime ?boolean|nil if the game should run in realtime or not (22.4 updates per second)
 function sc2ai:createGame(mapPath, enemyRace, autoJoin, disableFog, randomSeed, realtime)
 	if autoJoin == nil then
 		autoJoin = true
@@ -103,6 +122,7 @@ function sc2ai:createGame(mapPath, enemyRace, autoJoin, disableFog, randomSeed, 
 	end
 end
 
+---joins game after creating it 
 function sc2ai:joinGame()
 	local joinGameRequest = {
 		race = self.race,
@@ -126,6 +146,7 @@ local function parseAlliance(self,alliance)
 	end
 end
 
+--@nodoc
 local function generateComposition(unitTable) 
 
 	assert(unitTable , "unit table to generate composition is null")
@@ -154,7 +175,7 @@ local function generateComposition(unitTable)
 	table.insert(compTable, { type = lastType, count = counter } )
 	return compTable
 end
-
+--- gets the current state of the game (meaning number of resources and all visible units)
 function sc2ai:getGameState()
 
 	local gameState = self.connection:send({}, "observation").observation.observation
@@ -199,6 +220,10 @@ function sc2ai:getGameState()
 	self.alliedUnitComposition = generateComposition(self.alliedUnits)
 end
 
+---gets the number of an units visible on the map of specific type
+---@param unitType number id of the unit to get
+---@param alliance string the alliance of the unit "Self" for self, "Enemy" for enemies , "Ally" for allies and "Neutral" for neutral units (meaing mineral fields or barries on the map)
+---@return number count the number of units found
 function sc2ai:getUnitCount(unitType , alliance)
 	local searchTable 
 	if alliance == nil or alliance == "Self" then
@@ -217,6 +242,10 @@ function sc2ai:getUnitCount(unitType , alliance)
 	return 0
 end
 
+---gets unit by tag
+---@param unitTag number tag of the unit
+---@param alliance string the alliance of the unit "Self" for self, "Enemy" for enemies , "Ally" for allies and "Neutral" for neutral units (meaing mineral fields or barries on the map)
+---@return ?table|nil unit the unit with the tag or nil if nothing is found
 function sc2ai:findUnitByTag(unitTag , alliance)
 	
 	local searchTable
@@ -251,8 +280,11 @@ function sc2ai:findUnitByTag(unitTag , alliance)
 end
 
 
-
-
+---orders an unit to attack 
+---@param unitTag number the tag of the unit which will be moving
+---@param orderTarget ?number|table the tag of the unit to be moved to or if passed as coordinates the place where to go 
+---@param queueOrder ?boolean|nil queuing the order meaning doing the same thing as in base game holding shift and giving the order
+---@return ?boolean|string result of the action , if successful it will return true , otherwise it will return a string stating the reason for failure
 function sc2ai:orderMove(unitTag , orderTarget, queueOrder)
 	if queueOrder == nil then
 		queueOrder = false
@@ -268,6 +300,12 @@ function sc2ai:orderMove(unitTag , orderTarget, queueOrder)
 	return true
 end
 
+
+---orders an unit to attack 
+---@param unitTag number the tag of the unit which will be attacking
+---@param orderTarget ?number|table the tag of the unit to be attacked or if passed as coordinates the place where to attack
+---@param queueOrder ?boolean|nil queuing the order meaning doing the same thing as in base game holding shift and giving the order
+---@return ?boolean|string result of the action , if successful it will return true , otherwise it will return a string stating the reason for failure
 function sc2ai:orderAttack(unitTag , orderTarget , queueOrder)
 	if queueOrder == nil then
 		queueOrder = false
@@ -283,6 +321,12 @@ function sc2ai:orderAttack(unitTag , orderTarget , queueOrder)
 	return true
 end
 
+---orders a worker to build a structure
+---@param unitTag number the tag of the unit which will build
+---@param orderTarget ?table|number tag of the unit on which the structure will be built, if passed as a coordinate table { x , y } the place where the building will be built
+---@param unitToBuildId number the id of the type of unit to be built
+---@param queueOrder ?boolean|nil queuing the order meaning doing the same thing as in base game holding shift and giving the order
+---@return ?boolean|string result of the action , if successful it will return true , otherwise it will return a string stating the reason for failure
 function sc2ai:orderBuild(unitTag , orderTarget ,  unitToBuildId , queueOrder)
 	if queueOrder == nil then
 		queueOrder = false
@@ -300,8 +344,12 @@ function sc2ai:orderBuild(unitTag , orderTarget ,  unitToBuildId , queueOrder)
 	return true
 end
 
+---Uses special ability 
+---@param unitTag number the tag of the unit using the ability
+---@param abilityId number the id of the ability that is to be used 
+---@param orderTarget ?table|number tag of the unit on which the ability is to be used , if passed as a coordinate table { x , y } the place where the ability will be targeted 
+---@return ?boolean|string result of the action , if successful it will return true , otherwise it will return a string stating the reason for failure
 function sc2ai:useSpecialAbility(unitTag ,abilityId , orderTarget) 
-
 	local unit = self:findUnitByTag(unitTag)
 
 	assert(unit ~= nil , "unable to find unit with tag of" .. unitTag)
@@ -314,6 +362,10 @@ function sc2ai:useSpecialAbility(unitTag ,abilityId , orderTarget)
 	return true
 end
 
+---Trains an unit
+---@param trainerTag number the tag of the trainer unit
+---@param unitId number the id of the type of unit which is to be trained
+---@return ?boolean|string result of the action , if successful it will return true , otherwise it will return a string stating the reason for failure
 function sc2ai:trainUnit(trainerTag , unitId)
 	local unit = self:findUnitByTag(trainerTag)
 	assert(unit ~= nil , "unable to find unit with tag of " .. trainerTag)
@@ -333,7 +385,8 @@ function sc2ai:trainUnit(trainerTag , unitId)
 end
 
 
-
+---main loop for running the behaviour of the agent
+---@param callback function the callback function where the behaviour of the agent is defined
 function sc2ai:Loop(callback)
 	--!FIXME: do this correctly ! await the start of the game from start of countdown
 	socket.sleep(4)
@@ -350,6 +403,10 @@ function sc2ai:Loop(callback)
 	end
 end
 
+---finds a single unit 
+---@param unitType number id of the unit type
+---@param alliance string the alliance of the unit being searched for "Self" for self, "Enemy" for enemies , "Ally" for allies and "Neutral" for neutral units (meaing mineral fields or barries on the map)
+---@return ?table|nil unit a single unit of the type or nil if none are found
 function sc2ai:findUnitByType(unitType, alliance)
 	local searchTable = parseAlliance(self,alliance)
 	for _,unit in pairs(searchTable) do 
@@ -360,7 +417,10 @@ function sc2ai:findUnitByType(unitType, alliance)
 	return nil
 end
 
-
+---gets all units of a type
+---@param unitType number id of the unit type
+---@param alliance string the alliance of the unit being searched for "Self" for self, "Enemy" for enemies , "Ally" for allies and "Neutral" for neutral units (meaing mineral fields or barries on the map)
+---@return ?table|nil units a table of units of the type or nil if none are found
 function sc2ai:getAllUnitsOfType(unitType , alliance)
 	local searchTable = parseAlliance(self,alliance)
 	local res = {}
@@ -377,6 +437,12 @@ function sc2ai:getAllUnitsOfType(unitType , alliance)
 	return nil
 end
 
+---finds the neartest unit of type from a point or an unit
+---@param target ?table|number the coordinates { x , y } or a tag of an unit , if passed in as a tag it will use the units coordinatesand find the neartest to it 
+---@param unitType number which type of an unit to find
+---@param alliance string the alliance of the unit being searched for "Self" for self, "Enemy" for enemies , "Ally" for allies and "Neutral" for neutral units (meaing mineral fields or barries on the map)
+---@param targetAlliance ?string|nil the alliance of the unit we are searching from , if the target is given as a table of coordinates then this parameter is ignored 
+---@return ?table|nil the nearest unit found or nil if no unit found
 function sc2ai:findNearestUnitOfType(target , unitType, alliance , targetAlliance)
 	if type(target) ~= "table" then 
 		local unit = self:findUnitByTag(target, targetAlliance)
@@ -404,6 +470,9 @@ function sc2ai:findNearestUnitOfType(target , unitType, alliance , targetAllianc
 	return minUnit
 end
 
+---gets the unit with the lowest number of orders of a certain type 
+---@param unitType number the type of the unit
+---@return table unit the unit with the lowest number of orders
 function sc2ai:getLeastOrderedUnitOfType(unitType)
 	local minUnit = nil
 	local minOrders = math.huge
@@ -421,6 +490,9 @@ function sc2ai:getLeastOrderedUnitOfType(unitType)
 	return minUnit
 end
 
+---gets all ide Units of a type
+---@param unitType number the type of the unit
+---@return ?table|nil an array of all idle units , if no idle units exist it returns nil
 function sc2ai:getIdleUnitsOfType(unitType)
 	local res = {}		
 	for _, unit in pairs(self.units) do
@@ -434,7 +506,8 @@ function sc2ai:getIdleUnitsOfType(unitType)
 	return nil
 end
 
-
+---gets all ide Units
+---@return ?table|nil an array of all idle units , if no idle units exist it returns nil
 function sc2ai:getIdleUnits()
 	local res = {}		
 	for _, unit in pairs(self.units) do
@@ -447,7 +520,7 @@ function sc2ai:getIdleUnits()
 	end
 	return nil
 end
-
+---quits the game and forces starcraft 2 to shutdown
 function sc2ai:quitGame()
 	self.connection:send({}, "quit")
 end
